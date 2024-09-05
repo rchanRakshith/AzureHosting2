70a9110d59f899a4933b293c56d8df493a7056aa
rchanRakshith <165991897+rchanRakshith@users.noreply.github.com>
2024-09-05 14:04:18 +0530

    done changes in program.cs

--- FrontEnd/BlazorAppHosting2/Program.cs
+++ FrontEnd/BlazorAppHosting2/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSingleton<WeatherForecastService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+ // development removed
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
